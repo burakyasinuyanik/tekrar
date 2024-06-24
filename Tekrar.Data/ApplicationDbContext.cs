cdf766034f887b9361d9b4c3fdd7974e2b9ed039
Burak Yasin Uyanık <burakyasin3370@gmail.com>
2024-06-24 21:38:57 +0300

    Add project files.

---
+++ Tekrar.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekrar.Models;
+
+namespace Tekrar.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Tekrar.Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekrar.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; } = DateTime.Now;
+    }
+}
